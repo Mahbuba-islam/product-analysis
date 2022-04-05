b3a0175aa7beef8dfa2bc60a93a02b373ceae2b0
mahbuba <mahbubaislam7010@gmail.com>
2022-04-05 03:51:17 -0400

    load-review-data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Home.css'
+
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-section'>
+      <div className="product-container">
+      <div className="product-info">
+       <h1>Our Gorgeous Selection of Matte Lipstick for Women of color</h1>
+       <p>Add the finishing touch to any beauty look with one of our velvet matte lipsticks. Whether your go-to lip is a beautiful nude or a bright shade of red, each of Mented’s lipsticks are perfectly pigmented with all day staying power and packed with a formula that locks in moisture. we want every woman to feel beautiful, no matter her skin tone. To help you do that, we’ve created a complete collection of matte lipstick shades for women of color. </p>
+      </div>
+      <div className="picture">
+      <img src="../image/pexels-valeria-boltneva-lipistic.jpg" alt="" />
+      </div>
+       
       
+      </div>
+      
+      <div className="review-container">
+
+      </div>
+
     </div>
   );
 };
